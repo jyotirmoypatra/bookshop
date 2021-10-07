23716f16077058105fcbdba33f3640222ce8e8f8
jyotirmoypatra <jyotirmoypatra1@gmail.com>
2021-10-07 21:02:50 +0530

    modify responsiveness

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+
+searchForm=document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick=()=>{
+  searchForm.classList.toggle('active');
+}
+
+window.onscroll=()=>{
+  searchForm.classList.remove('active');
+}
+
+
+
+
+
 var swiper = new Swiper(".book-slider", {
     
     
